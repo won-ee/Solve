d5167fa87ba4335ea5130a0b0654af923e1dcb70
won-ee <156388848+won-ee@users.noreply.github.com>
2024-02-23 19:26:02 +0900

    [D2] Title: 간단한 369게임, Time: 147 ms, Memory: 58,524 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
@@ -1,11 +1,8 @@
 n = int(input())
 for i in range(1,n+1):
     if '3' in list(str(i)) or '6' in list(str(i)) or '9' in list(str(i)):
-        bak =0
-        bak += list(str(i)).count('3')
-        bak += list(str(i)).count('6')
-        bak += list(str(i)).count('9')
-        a = '-'*bak
+        a = '-'
+        a *= (list(str(i)).count('3') + list(str(i)).count('6') + list(str(i)).count('9'))
         print(a,end=' ')
         continue
     print(i,end=' ')
