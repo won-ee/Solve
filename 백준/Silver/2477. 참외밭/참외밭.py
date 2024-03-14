73abe3d1605a5cf4ba997e39a9a380e9a426d091
won-ee <156388848+won-ee@users.noreply.github.com>
2024-03-14 21:30:19 +0900

    [Silver II] Title: 참외밭, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.py
+++ 백준/Silver/2477. 참외밭/참외밭.py
@@ -1,29 +1,28 @@
-K = int(input())
-max_height = 0  # 가장 긴 높이를 저장할 변수
-max_width = 0  # 가장 긴 가로길이를 저장할 변수
-max_width_index = 0  # 가장 긴 가로길이의 인덱스를 저장할 변수
-max_height_index = 0  # 가장 긴 높이의 인덱스를 저장할 변수
-info = []  # 변의 정보들을 저장할 리스트
+N = int(input())
+arr = []
+max_y,max_x = 0,0
+max_y_index,max_x_index = 0,0
+
+
 for i in range(6):
-    temp = list(map(int, input().split()))
-    info.append(temp)
-    if temp[0] == 1 or temp[0] == 2:  # 만약 가로 길이라면
-        if max_width < temp[1]:  # 해당 길이가 가장 길다면
-            max_width = temp[1]  # 그 길이를 가장긴 가로로 저장한 후
-            max_width_index = i  # 그에 해당하는 인덱스를 저장
+    a,b = map(int,input().split())
+    if a == 1 or a == 2:
+        if max_y<b:
+            max_y =b
+            max_y_index = i
     else:
-        if max_height < temp[1]:  # 해당 길이가 가장 길다면
-            max_height = temp[1]  # 그 길이를 가장긴 세로로 저장한 후
-            max_height_index = i  # 그에 해당하는 인덱스를 저장
+        if max_x <b:
+            max_x = b
+            max_x_index = i
+    arr.append((a,b))
+index_list = [arr[max_y_index - 1], arr[(max_y_index + 1) % 6], arr[max_x_index - 1],
+              arr[(max_x_index + 1) % 6]]
 
-# 그 후 가장 긴 가로 및 세로와 인접한 변 2개와 가장긴 가로와 세로 총 4개를 새로운 리스트에 저장한다.
-index_list = [info[max_height_index - 1], info[(max_height_index + 1) % 6], info[max_width_index - 1],
-              info[(max_width_index + 1) % 6]]
 
-product = 1  # 곱을 저장할 변수
-for i in info:  # 입력받은 변들 중에
-    if i not in index_list:  # 만약 새로운 리스트에 저장한 변 4개 중에 없다면 빈 사각형이므로
-        product *= i[1]  # 그 넓이를 변수에 저장한다.
+ans = 1
+for i in arr:
+    if i not in index_list:
+        ans *=i[1]
+print(N*(max_y*max_x-ans))
 
-result = (max_width * max_height - product) * K  # 전체 큰 직사각형 넓이에서 빈 사각형 넓이를 빼준 후 K를 곱한다.
-print(result)
+
