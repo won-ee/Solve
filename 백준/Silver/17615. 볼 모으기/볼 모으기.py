201bc452d1237952a7e4665ea0323099f6283b43
won-ee <156388848+won-ee@users.noreply.github.com>
2024-03-28 19:22:58 +0900

    [Silver I] Title: 볼 모으기, Time: 144 ms, Memory: 111784 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.py
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.py
@@ -1,6 +1,6 @@
+import sys
 n = int(input())
-br = input()
-a = len(br)-1
+br = sys.stdin.readline().rstrip()
 b,r = 0,0
 flag = 0
 for i in range(n-2,-1,-1):
@@ -8,8 +8,18 @@ for i in range(n-2,-1,-1):
         flag = 1
     if flag:
         if br[i] == br[-1]:
-             b+= 1
+             b += 1
         else:
             r +=1
+B,R = 0,0
+flag = 0
+for i in range(1,n):
+    if br[i] != br[0]:
+        flag = 1
+    if flag:
+        if br[i] == br[0]:
+            B += 1
+        else:
+            R += 1
 
-print(min(b,r))
+print(min(b,r,B,R))
