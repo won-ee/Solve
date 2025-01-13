b24c441a46bab0838a04c4a6fb902e21aa18713d
won-ee <156388848+won-ee@users.noreply.github.com>
2025-01-13 18:05:16 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 108 ms, Memory: 13108 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int arr[] = new int[N];
+        int temp = 0;
+        for (int i = 0; i < N; i++){
+            arr[i] = i + 1;
+        }
+
+        for (int i=0; i<M; i++){
+            int a = sc.nextInt() - 1;
+            int b = sc.nextInt() - 1;
+
+            while (a<b){
+                temp = arr[a];
+                arr[a] = arr[b];
+                arr[b] = temp;
+                a++;
+                b--;
+            }
+        }
+        for (int i = 0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
