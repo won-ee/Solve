aa46c25f451fc7a7d0a75025a7c2b4f8ca620026
won-ee <156388848+won-ee@users.noreply.github.com>
2025-04-07 17:53:15 +0900

    [Silver IV] Title: 주몽, Time: 128 ms, Memory: 15772 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,34 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        String[] arr = br.readLine().split(" ");
-        
+        String[] strArr = br.readLine().split(" ");
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(strArr[i]);
+
+        }
+        int left = 0;
+        int right = n-1;
         int cnt = 0;
-        
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == null) continue;
-            int a = Integer.parseInt(arr[i]);
-            
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] == null) continue;
-                int b = Integer.parseInt(arr[j]);
-                
-                if (a + b == m) {
-                    arr[i] = null;
-                    arr[j] = null;
-                    cnt++;
-                    break;
-                }
+        Arrays.sort(arr);
+        while (left<right){
+            int sum = arr[left] + arr[right];
+            if(sum<m){
+                left++;
+            } else if (sum>m) {
+                right--;
+            }else {
+                cnt ++;
+                left++;
+                right--;
             }
         }
-
         System.out.println(cnt);
     }
 }
