2b79e9906b897cca37a5cc63cd3403196ebe31e9
won-ee <156388848+won-ee@users.noreply.github.com>
2025-04-10 10:23:31 +0900

    [Gold IV] Title: 우체국, Time: 644 ms, Memory: 63152 KB -BaekjoonHub

--- 백준/Gold/2141. 우체국/우체국.java
+++ 백준/Gold/2141. 우체국/우체국.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -18,13 +17,7 @@ public class Main {
             arr[i][1] = Integer.parseInt(strArr[1]);
             mid += Integer.parseInt(strArr[1]);
         }
-        Arrays.sort(arr,(a,b) -> {
-            if (a[0] != b[0]){
-                return Integer.compare(a[0],b[0]);
-            }else {
-                return Integer.compare(a[1],b[1]);
-            }
-        });
+        Arrays.sort(arr,(a,b) -> Integer.compare(a[0],b[0]));
 
         long person = 0;
         for (int[] ar : arr){
