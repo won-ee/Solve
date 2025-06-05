bd481471843966017cf71ec7f976eb730a3185a2
won-ee <156388848+won-ee@users.noreply.github.com>
2025-06-05 19:32:31 +0900

    [Gold V] Title: 경쟁적 전염, Time: 188 ms, Memory: 20672 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
@@ -2,16 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, k;
-    static int[][] arr;
-    static int[] dx = {0, 0, 1, -1};
-    static int[] dy = {1, -1, 0, 0};
+    static int N, K, S, X, Y;
+    static int[][] graph;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
     static class Virus implements Comparable<Virus> {
-        int type, time, x, y;
-        public Virus(int type, int time, int x, int y) {
+        int type, x, y;
+
+        public Virus(int type, int x, int y) {
             this.type = type;
-            this.time = time;
             this.x = x;
             this.y = y;
         }
@@ -24,49 +24,56 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] nk = br.readLine().split(" ");
+        N = Integer.parseInt(nk[0]);
+        K = Integer.parseInt(nk[1]);
 
-        String[] nkStr = br.readLine().split(" ");
-        n = Integer.parseInt(nkStr[0]);
-        k = Integer.parseInt(nkStr[1]);
-
-        arr = new int[n][n];
+        graph = new int[N][N];
         List<Virus> virusList = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             String[] row = br.readLine().split(" ");
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(row[j]);
-                if (arr[i][j] != 0) {
-                    virusList.add(new Virus(arr[i][j], 0, i, j));
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = Integer.parseInt(row[j]);
+                if (graph[i][j] != 0) {
+                    virusList.add(new Virus(graph[i][j], i, j));
                 }
             }
         }
 
-        Collections.sort(virusList);
-
-        Queue<Virus> queue = new LinkedList<>(virusList);
-
         String[] sxy = br.readLine().split(" ");
-        int S = Integer.parseInt(sxy[0]);
-        int X = Integer.parseInt(sxy[1]) - 1;
-        int Y = Integer.parseInt(sxy[2]) - 1;
+        S = Integer.parseInt(sxy[0]);
+        X = Integer.parseInt(sxy[1]);
+        Y = Integer.parseInt(sxy[2]);
 
-        while (!queue.isEmpty()) {
-            Virus v = queue.poll();
-            if (v.time == S) break;
+        Collections.sort(virusList);
+        System.out.println(bfs(virusList));
+    }
 
-            for (int i = 0; i < 4; i++) {
-                int nx = v.x + dx[i];
-                int ny = v.y + dy[i];
-                if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
-                    if (arr[nx][ny] == 0) {
-                        arr[nx][ny] = v.type;
-                        queue.offer(new Virus(v.type, v.time + 1, nx, ny));
+    static int bfs(List<Virus> virusList) {
+        Queue<Virus> q = new LinkedList<>(virusList);
+        int time = 0;
+
+        while (!q.isEmpty()) {
+            if (time == S) break;
+            int size = q.size(); 
+
+            for (int i = 0; i < size; i++) {
+                Virus v = q.poll();
+
+                for (int d = 0; d < 4; d++) {
+                    int nx = v.x + dx[d];
+                    int ny = v.y + dy[d];
+
+                    if (nx >= 0 && ny >= 0 && nx < N && ny < N && graph[nx][ny] == 0) {
+                        graph[nx][ny] = v.type;
+                        q.offer(new Virus(v.type, nx, ny));
                     }
                 }
             }
+            time++;
         }
 
-        System.out.println(arr[X][Y]);
+        return graph[X - 1][Y - 1];
     }
 }
